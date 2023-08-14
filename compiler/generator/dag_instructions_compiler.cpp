aherrou/faust

Rework -fx mode.

compiler/generator/dag_instructions_compiler.cpp
@@ -274,7 +274,7 @@ ValueInst* DAGInstructionsCompiler::generateLoopCode(Tree sig)
 ValueInst* DAGInstructionsCompiler::generateCacheCode(Tree sig, ValueInst* exp)
 {
     string         vname;
-    Typed::VarType ctype;
+    BasicTyped*    ctype;
     int            sharing = getSharingCount(sig, fSharingKey);
     ::Type         t       = getCertifiedSigType(sig);
     Occurrences*   o       = fOccMarkup->retrieve(sig);
@@ -394,7 +394,7 @@ ValueInst* DAGInstructionsCompiler::generateVariableStore(Tree sig, ValueInst* e
 
     if (t->variability() == kSamp) {
         string         vname;
-        Typed::VarType ctype;
+        BasicTyped* ctype;
         getTypedNames(t, "Vector", ctype, vname);
         Address::AccessType var_access;
         generateVectorLoop(ctype, vname, exp, var_access);
@@ -486,7 +486,7 @@ ValueInst* DAGInstructionsCompiler::generateDelay(Tree sig, Tree exp, Tree delay
     }
 }
 
-ValueInst* DAGInstructionsCompiler::generateDelayVec(Tree sig, ValueInst* exp, Typed::VarType ctype,
+ValueInst* DAGInstructionsCompiler::generateDelayVec(Tree sig, ValueInst* exp, BasicTyped* ctype,
                                                      const string& vname, int mxd)
 {
     // it is a non-sample but used delayed
@@ -503,7 +503,7 @@ ValueInst* DAGInstructionsCompiler::generateDelayVec(Tree sig, ValueInst* exp, T
     }
 }
 
-ValueInst* DAGInstructionsCompiler::generateDelayLine(ValueInst* exp, Typed::VarType ctype, const string& vname,
+ValueInst* DAGInstructionsCompiler::generateDelayLine(ValueInst* exp, BasicTyped* ctype, const string& vname,
                                                       int mxd, Address::AccessType& var_access, ValueInst* unused)
 {
     if (mxd == 0) {
@@ -515,12 +515,12 @@ ValueInst* DAGInstructionsCompiler::generateDelayLine(ValueInst* exp, Typed::Var
     return exp;
 }
 
-void DAGInstructionsCompiler::generateVectorLoop(Typed::VarType ctype, const string& vname, ValueInst* exp,
-                                                 Address::AccessType& var_access)
+void DAGInstructionsCompiler::generateVectorLoop(BasicTyped* ctype, const string& vname, ValueInst* exp,
+                                            Address::AccessType& var_access)
 {
     // "$0 $1[$2];"
     DeclareVarInst* table_inst = InstBuilder::genDecStackVar(
-        vname, InstBuilder::genArrayTyped(InstBuilder::genBasicTyped(ctype), gGlobal->gVecSize));
+        vname, InstBuilder::genArrayTyped(ctype, gGlobal->gVecSize));
     pushComputeBlockMethod(table_inst);
 
     // -- compute the new samples
@@ -531,10 +531,10 @@ void DAGInstructionsCompiler::generateVectorLoop(Typed::VarType ctype, const str
     var_access = Address::kStack;
 }
 
-void DAGInstructionsCompiler::generateDlineLoop(Typed::VarType ctype, const string& vname, int delay, ValueInst* exp,
-                                                Address::AccessType& var_access)
+void DAGInstructionsCompiler::generateDlineLoop(BasicTyped* ctype, const string& vname, int delay, ValueInst* exp,
+                                            Address::AccessType& var_access)
 {
-    BasicTyped* typed = InstBuilder::genBasicTyped(ctype);
+    BasicTyped* typed = ctype;
 
     if (delay < gGlobal->gMaxCopyDelay) {
         // Implementation of a copy based delayline
@@ -609,8 +609,7 @@ void DAGInstructionsCompiler::generateDlineLoop(Typed::VarType ctype, const stri
     }
 }
 
-StatementInst* DAGInstructionsCompiler::generateCopyBackArray(const string& vname_to, const string& vname_from,
-                                                              int size)
+StatementInst* DAGInstructionsCompiler::generateCopyBackArray(const string& vname_to, const string& vname_from, int size)
 {
     string index = gGlobal->getFreshID("j");
 

compiler/generator/dag_instructions_compiler.hh
@@ -28,31 +28,31 @@ class DAGInstructionsCompiler : public InstructionsCompiler {
    public:
     DAGInstructionsCompiler(CodeContainer* container);
 
-    virtual void compileMultiSignal(Tree sig);
+    virtual void compileMultiSignal(Tree sig) override;
 
    private:
     // reimplemented code generation methods
-    virtual ValueInst* CS(Tree sig);
-    virtual ValueInst* generateCode(Tree sig);
+    virtual ValueInst* CS(Tree sig) override;
+    virtual ValueInst* generateCode(Tree sig) override;
     virtual void       generateCodeRecursions(Tree sig);
     virtual ValueInst* generateCodeNonRec(Tree sig);
     virtual ValueInst* generateLoopCode(Tree sig);
 
-    virtual ValueInst* generateWaveform(Tree sig);
+    virtual ValueInst* generateWaveform(Tree sig) override;
 
-    void generateVectorLoop(Typed::VarType ctype, const std::string& vecname, ValueInst* exp,
+    void generateVectorLoop(BasicTyped* ctype, const std::string& vecname, ValueInst* exp,
                             Address::AccessType& var_access);
-    void generateDlineLoop(Typed::VarType ctype, const std::string& vecname, int delay, ValueInst* exp,
-                           Address::AccessType& var_access);
+    void generateDlineLoop(BasicTyped* ctype, const std::string& vecname, int delay, ValueInst* exp,
+                            Address::AccessType& var_access);
 
-    virtual ValueInst* generateVariableStore(Tree sig, ValueInst* inst);
-    virtual ValueInst* generateCacheCode(Tree sig, ValueInst* inst);
-    virtual ValueInst* generateInput(Tree sig, int idx);
+    virtual ValueInst* generateVariableStore(Tree sig, ValueInst* inst) override;
+    virtual ValueInst* generateCacheCode(Tree sig, ValueInst* inst) override;
+    virtual ValueInst* generateInput(Tree sig, int idx) override;
 
-    virtual ValueInst* generateDelay(Tree sig, Tree arg, Tree size);
-    virtual ValueInst* generateDelayVec(Tree sig, ValueInst* exp, Typed::VarType ctype, const std::string& vname, int mxd);
-    virtual ValueInst* generateDelayLine(ValueInst* exp, Typed::VarType ctype, const std::string& vname, int mxd,
-                                         Address::AccessType& var_access, ValueInst* ccs);
+    virtual ValueInst* generateDelay(Tree sig, Tree arg, Tree size) override;
+    virtual ValueInst* generateDelayVec(Tree sig, ValueInst* exp, BasicTyped* ctype, const std::string& vname, int mxd) override;
+    virtual ValueInst* generateDelayLine(ValueInst* exp,  BasicTyped* ctype, const std::string& vname, int mxd,
+                                         Address::AccessType& var_access, ValueInst* ccs) override;
 
     StatementInst* generateCopyBackArray(const std::string& vname_to, const std::string& vname_from, int size);
 

sthagen/grame-cncm-faust

compiler/generator/dag_instructions_compiler.cpp
@@ -274,7 +274,7 @@ ValueInst* DAGInstructionsCompiler::generateLoopCode(Tree sig)
 ValueInst* DAGInstructionsCompiler::generateCacheCode(Tree sig, ValueInst* exp)
 {
     string         vname;
-    Typed::VarType ctype;
+    BasicTyped*    ctype;
     int            sharing = getSharingCount(sig, fSharingKey);
     ::Type         t       = getCertifiedSigType(sig);
     Occurrences*   o       = fOccMarkup->retrieve(sig);
@@ -394,7 +394,7 @@ ValueInst* DAGInstructionsCompiler::generateVariableStore(Tree sig, ValueInst* e
 
     if (t->variability() == kSamp) {
         string         vname;
-        Typed::VarType ctype;
+        BasicTyped* ctype;
         getTypedNames(t, "Vector", ctype, vname);
         Address::AccessType access;
         generateVectorLoop(ctype, vname, exp, access);
@@ -486,7 +486,7 @@ ValueInst* DAGInstructionsCompiler::generateDelay(Tree sig, Tree exp, Tree delay
     }
 }
 
-ValueInst* DAGInstructionsCompiler::generateDelayVec(Tree sig, ValueInst* exp, Typed::VarType ctype,
+ValueInst* DAGInstructionsCompiler::generateDelayVec(Tree sig, ValueInst* exp, BasicTyped* ctype,
                                                      const string& vname, int mxd)
 {
     // it is a non-sample but used delayed
@@ -503,7 +503,7 @@ ValueInst* DAGInstructionsCompiler::generateDelayVec(Tree sig, ValueInst* exp, T
     }
 }
 
-ValueInst* DAGInstructionsCompiler::generateDelayLine(ValueInst* exp, Typed::VarType ctype, const string& vname,
+ValueInst* DAGInstructionsCompiler::generateDelayLine(ValueInst* exp, BasicTyped* ctype, const string& vname,
                                                       int mxd, Address::AccessType& access, ValueInst* unused)
 {
     if (mxd == 0) {
@@ -515,12 +515,12 @@ ValueInst* DAGInstructionsCompiler::generateDelayLine(ValueInst* exp, Typed::Var
     return exp;
 }
 
-void DAGInstructionsCompiler::generateVectorLoop(Typed::VarType ctype, const string& vname, ValueInst* exp,
-                                                 Address::AccessType& access)
+void DAGInstructionsCompiler::generateVectorLoop(BasicTyped* ctype, const string& vname, ValueInst* exp,
+                                            Address::AccessType& access)
 {
     // "$0 $1[$2];"
     DeclareVarInst* table_inst = InstBuilder::genDecStackVar(
-        vname, InstBuilder::genArrayTyped(InstBuilder::genBasicTyped(ctype), gGlobal->gVecSize));
+        vname, InstBuilder::genArrayTyped(ctype, gGlobal->gVecSize));
     pushComputeBlockMethod(table_inst);
 
     // -- compute the new samples
@@ -531,10 +531,10 @@ void DAGInstructionsCompiler::generateVectorLoop(Typed::VarType ctype, const str
     access = Address::kStack;
 }
 
-void DAGInstructionsCompiler::generateDlineLoop(Typed::VarType ctype, const string& vname, int delay, ValueInst* exp,
-                                                Address::AccessType& access)
+void DAGInstructionsCompiler::generateDlineLoop(BasicTyped* ctype, const string& vname, int delay, ValueInst* exp,
+                                            Address::AccessType& access)
 {
-    BasicTyped* typed = InstBuilder::genBasicTyped(ctype);
+    BasicTyped* typed = ctype;
 
     if (delay < gGlobal->gMaxCopyDelay) {
         // Implementation of a copy based delayline
@@ -609,8 +609,7 @@ void DAGInstructionsCompiler::generateDlineLoop(Typed::VarType ctype, const stri
     }
 }
 
-StatementInst* DAGInstructionsCompiler::generateCopyBackArray(const string& vname_to, const string& vname_from,
-                                                              int size)
+StatementInst* DAGInstructionsCompiler::generateCopyBackArray(const string& vname_to, const string& vname_from, int size)
 {
     string index = gGlobal->getFreshID("j");
 

compiler/generator/dag_instructions_compiler.hh
@@ -28,34 +28,34 @@ class DAGInstructionsCompiler : public InstructionsCompiler {
    public:
     DAGInstructionsCompiler(CodeContainer* container);
 
-    virtual void compileMultiSignal(Tree sig);
+    virtual void compileMultiSignal(Tree sig) override;
 
    protected:
-    // reimplemented code generation methods
-    virtual ValueInst* CS(Tree sig);
+    
+    virtual ValueInst* CS(Tree sig) override;
 
    private:
-    virtual ValueInst* generateCode(Tree sig);
+   
+    virtual ValueInst* generateCode(Tree sig) override;
     virtual void       generateCodeRecursions(Tree sig);
     virtual ValueInst* generateCodeNonRec(Tree sig);
     virtual ValueInst* generateLoopCode(Tree sig);
 
-    virtual ValueInst* generateWaveform(Tree sig);
+    virtual ValueInst* generateWaveform(Tree sig) override;
 
-    void generateVectorLoop(Typed::VarType ctype, const std::string& vecname, ValueInst* exp,
+    void generateVectorLoop(BasicTyped* ctype, const std::string& vecname, ValueInst* exp,
                             Address::AccessType& access);
-    void generateDlineLoop(Typed::VarType ctype, const std::string& vecname, int delay, ValueInst* exp,
-                           Address::AccessType& access);
+    void generateDlineLoop(BasicTyped* ctype, const std::string& vecname, int delay, ValueInst* exp,
+                            Address::AccessType& access);
 
-    virtual ValueInst* generateVariableStore(Tree sig, ValueInst* inst);
-    virtual ValueInst* generateCacheCode(Tree sig, ValueInst* inst);
-    virtual ValueInst* generateInput(Tree sig, int idx);
+    virtual ValueInst* generateVariableStore(Tree sig, ValueInst* inst) override;
+    virtual ValueInst* generateCacheCode(Tree sig, ValueInst* inst) override;
+    virtual ValueInst* generateInput(Tree sig, int idx) override;
 
-    virtual ValueInst* generateDelay(Tree sig, Tree arg, Tree size);
-    virtual ValueInst* generateDelayVec(Tree sig, ValueInst* exp, Typed::VarType ctype, const std::string& vname,
-                                        int mxd);
-    virtual ValueInst* generateDelayLine(ValueInst* exp, Typed::VarType ctype, const std::string& vname, int mxd,
-                                         Address::AccessType& access, ValueInst* ccs);
+    virtual ValueInst* generateDelay(Tree sig, Tree arg, Tree size) override;
+    virtual ValueInst* generateDelayVec(Tree sig, ValueInst* exp, BasicTyped* ctype, const std::string& vname, int mxd) override;
+    virtual ValueInst* generateDelayLine(ValueInst* exp, BasicTyped* ctype, const std::string& vname, int mxd,
+                                         Address::AccessType& access, ValueInst* ccs) override;
 
     StatementInst* generateCopyBackArray(const std::string& vname_to, const std::string& vname_from, int size);
 
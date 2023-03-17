cea0ce188fb6229d550428db1fe4a4bb94399e25
Stephane Letz <letz@grame.fr>
2023-03-17 12:31:58 +0100

    Rework -fx mode.

--- compiler/transform/sigPromotion.hh
+++ compiler/transform/sigPromotion.hh
@@ -240,5 +240,5 @@ Tree signalTablePromote(Tree sig);
 Tree signalIntCastPromote(Tree sig);
 Tree signalUIPromote(Tree sig);
 Tree signalUIFreezePromote(Tree sig);
-Tree signalFTZPromotion(Tree sig);
+Tree signalFTZPromote(Tree sig);
 #endif
